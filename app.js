cb329dd42282962599a27ae8b974717ca0ae79bc
kelvinson <kelvinson@foxmail.com>
2018-01-07 03:50:22 -0800

    add form post method(using body-parser)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express'); //to use a node Module
 const path = require('path'); //another module 'path' which is a core node module
+const bodyParser = require('body-parser'); //use the 'body-parser' module
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 //server static files and set static path
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,6 +32,25 @@ app.get('/users', (req, res) => {
       }
     ];
     res.json(users);
+});
+
+//server pdf files using res.download API
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname,'/downloads/TTCG.pdf'));
+});
+
+//express redirect page
+app.get('/about', (req, res) => {
+  res.redirect('/about.html');
+});
+
+//post the form request defined in the index.html
+app.post('/subscribe', (req, res) => {
+  //rember to use 'body-parse' module before use the json data;
+  let name = req.body.name;
+  let email = req.body.email;
+
+  console.log(name + ' has subscribe with ' + email);
 });
 
 app.listen(3000, function(){
