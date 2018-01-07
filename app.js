5dc0226b35cf1c6d9b904428744e708db419869d
kelvinson <kelvinson@foxmail.com>
2018-01-07 03:13:08 -0800

    server json file with REST API

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ const app = express();
 //server static files and set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+//server json file, REST API
+app.get('/users', (req, res) => {
+    let users = [
+      {
+        first_name: "John",
+        last_name: "Doe",
+        age: 34,
+        gender: "male"
+      },
+      {
+        first_name: "Tom",
+        last_name: "Jackson",
+        age: 23,
+        gender: "male"
+      },
+      {
+        first_name: "Tracy",
+        last_name:"Smith",
+        age: 38,
+        gender: "female"
+      }
+    ];
+    res.json(users);
+});
+
 app.listen(3000, function(){
   console.log('Server started on port 3000');
 });
